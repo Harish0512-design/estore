0e6988efcd9438dc508195ff750113e9fcdf006a
Harish Somsole <66076559+Harish0512-design@users.noreply.github.com>
2023-09-01 15:53:10 +0530

    Update settings.py

--- estore/settings.py
+++ estore/settings.py
@@ -137,6 +137,6 @@ EMAIL_BCC = 'collegecirculars123@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'Harish'
-EMAIL_HOST_PASSWORD = 'Harish@python3'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
