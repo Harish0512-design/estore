7407f8fcc0889b85e6acc1096a5686b3baef1977
harish <harishsomsole@gmail.com>
2023-09-02 14:39:07 +0530

    added permission for productsAPI

--- product/views.py
+++ product/views.py
@@ -1,10 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import permissions
 from product.models import Product
 from product.myserializer import ProductSerializer
 
 
 # Create your views here.
 class ProductViewSet(ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
