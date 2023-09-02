a249e35201b243fa35fe821b5a5e466ef7d34dbd
Harish Somsole <66076559+Harish0512-design@users.noreply.github.com>
2023-09-02 14:47:09 +0530

    Harish (#5)
    * added registration, login, logout, password reset,change
    
    * added permission for productsAPI

--- product/views.py
+++ product/views.py
@@ -1,10 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import permissions
 from product.models import Product
 from product.myserializer import ProductSerializer
 
 
 # Create your views here.
 class ProductViewSet(ModelViewSet):
+<<<<<<< HEAD
+    permission_classes = [permissions.IsAuthenticated]
+=======
+>>>>>>> 0b90fc02c936df97fc0742e622f2b66581bcb74a
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
