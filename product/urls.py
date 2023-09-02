142db8f1c79dafbc242ac003b8eba755cb176ffc
Harish Somsole <66076559+Harish0512-design@users.noreply.github.com>
2023-09-02 12:39:56 +0530

    added registration, login, logout, password reset,change (#1)

--- product/urls.py
+++ product/urls.py
@@ -1,16 +1,11 @@
-from django.contrib import admin
 from django.urls import path, include
-from .views import UserRegisterView, PasswordChangeView
 from rest_framework.routers import DefaultRouter
 
-from product.views import ProductViewSet, UserRegisterView
+from product.views import ProductViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/accounts/', include('authemail.urls'))
-    # path('api/accounts/', UserRegisterView.as_view()),
-    # path('api/accounts/change-password', PasswordChangeView.as_view()),
 ]
